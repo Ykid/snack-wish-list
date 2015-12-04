8677d40420332361fb5356fc24bce6980daa76c5
David <choiwaikit.cwj@gmail.com>
2015-12-04 15:29:55 +0800

    change the body to 100% height

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,10 +4,23 @@ angular.module('altitudeLabsApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
     var sample = [{
+      name:'Snacks 1',
+      amount: 2,
+      imageUrl: 'https://www.geppettogroup.com/wp-content/uploads/2011/05/Scooby-Snacks1.jpg'
+    }, {
+      name:'Snacks 2',
+      amount: 3,
+      imageUrl: 'http://www.irvineparkrailroad.com/images/heroshots/12134826533.jpg'
+    }, {
+      name:'Snacks 3',
+      amount: 4,
+      imageUrl: 'http://www.firstuccdc.org/wp-content/uploads/2013/07/snacks.png'
     }];
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    $scope.awesomeThings = sample;
+
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
