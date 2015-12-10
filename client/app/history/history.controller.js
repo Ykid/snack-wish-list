243da92d9c053cd7138594d880a4b6efa6f3fa77
David <choiwaikit.cwj@gmail.com>
2015-12-11 02:00:18 +0800

    hook up get historial shopping entry api

--- client/app/history/history.controller.js
+++ client/app/history/history.controller.js
@@ -1,41 +1,47 @@
 'use strict';
 
 angular.module('altitudeLabsApp')
-  .controller('HistoryCtrl', function ($scope, $http, $window, $uibModal, $log, snackItemService) {
+  .controller('HistoryCtrl', function ($scope, $http, $window, $uibModal, $log, snackItemService, snackShoppingEntryService, Modal) {
     $scope.filterName = '';
-    var sample = [{
-      _id: '112',
-      snackName:'Snacks Name',
-      requestAmount: 2,
-      snackImageUrl: 'https://www.geppettogroup.com/wp-content/uploads/2011/05/Scooby-Snacks1.jpg',
-      likes: 5,
-      price: 12,
-      requester: 'Leonard',
-      availableLocaltions: ['park and shop', 'sasa', 'joyo', 'park and shop', 'park and shop', 'park and shop'],
-      requestedTimes: 1
-    }, {
-      _id: '113',
-      snackName:'Snacks 2 Name',
-      requestAmount: 3,
-      snackImageUrl: 'http://www.irvineparkrailroad.com/images/heroshots/12134826533.jpg',
-      likes: 2,
-      price: 24,
-      requester: 'Anthony',
-      availableLocaltions: ['711'],
-      requestedTimes: 20
-    }, {
-      _id: '111',
-      snackName:'Snacks 3 Name',
-      requestAmount: 4,
-      snackImageUrl: 'http://www.firstuccdc.org/wp-content/uploads/2013/07/snacks.png',
-      likes: 1,
-      price: 13,
-      requester: 'Andrianto',
-      availableLocaltions: ['759'],
-      requestedTimes: 3
-    }];
-    $scope.snackList = sample;
-
+    // var sample = [{
+    //   _id: '112',
+    //   snackName:'Snacks Name',
+    //   requestAmount: 2,
+    //   snackImageUrl: 'https://www.geppettogroup.com/wp-content/uploads/2011/05/Scooby-Snacks1.jpg',
+    //   likes: 5,
+    //   price: 12,
+    //   requester: 'Leonard',
+    //   availableLocaltions: ['park and shop', 'sasa', 'joyo', 'park and shop', 'park and shop', 'park and shop'],
+    //   requestedTimes: 1
+    // }, {
+    //   _id: '113',
+    //   snackName:'Snacks 2 Name',
+    //   requestAmount: 3,
+    //   snackImageUrl: 'http://www.irvineparkrailroad.com/images/heroshots/12134826533.jpg',
+    //   likes: 2,
+    //   price: 24,
+    //   requester: 'Anthony',
+    //   availableLocaltions: ['711'],
+    //   requestedTimes: 20
+    // }, {
+    //   _id: '111',
+    //   snackName:'Snacks 3 Name',
+    //   requestAmount: 4,
+    //   snackImageUrl: 'http://www.firstuccdc.org/wp-content/uploads/2013/07/snacks.png',
+    //   likes: 1,
+    //   price: 13,
+    //   requester: 'Andrianto',
+    //   availableLocaltions: ['759'],
+    //   requestedTimes: 3
+    // }];
+    $scope.snackList = [];
+    var showError = Modal.confirm.showError();
+    snackShoppingEntryService.getEntries('history').then(
+      function (successResponse) {
+        $scope.snackList = successResponse;
+      }, function (errorMsg) {
+        showError(errorMsg);
+      });
     //change the display of the button according to the
     //like status
     $scope.checkLikeStatus = function (snackItem) {
